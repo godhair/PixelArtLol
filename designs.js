9907c569a82732e75c2314b48f77abacbee3816c
godhair <73728503+godhair@users.noreply.github.com>
2020-10-30 23:02:12 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,41 +1,30 @@
-//variable declarations
+//variables
 const table = document.getElementById("pixel_canvas");
 const colorPicker = document.getElementById("colorPicker");
-let gridHeight, gridWidth;
+var Height, Width;
 
-//draw the grid when Submit is clicked or Enter/Return key is pressed
+//grid drawing
 const sizePicker = document.querySelector("#sizePicker");
 sizePicker.addEventListener("submit", function(e) {
     e.preventDefault();
     makeGrid();
 });
 
-/**
- * @description Draws the grid based on the user input for grid height and width
- * @param void
- * @return void
- */
 function makeGrid() {
 
-  //clear the existing table rows
+  //clear
   table.innerHTML = '';
 
-  //get the user input values for grid height and grid width
+  //obtain values for height and width
   gridHeight = document.getElementById("input-height").value;
   gridWidth = document.getElementById("input-width").value;
-
-  //draw table grid and add event listener for each cell
-  for (let i = 0; i < gridHeight; i++) {
+  for (let i = 0; i < Height; i++) {
     let row = table.insertRow(i);
-    for (let j = 0; j < gridWidth; j++) {
+    for (let j = 0; j < Width; j++) {
       let cell = row.insertCell(j);
-
-      //to change background color when user clicks
       cell.addEventListener("click", function(event) {
         cell.style.backgroundColor = colorPicker.value;
       });
-
-      //to remove the existing color when user double clicks
       cell.addEventListener("dblclick", function(event){
       	cell.style.backgroundColor = "";
       });
